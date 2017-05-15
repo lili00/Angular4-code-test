edd74d8491ee3ba8e68a3f3dc483d5f2446b8a9c
yiaarons <lily.li8899@gmail.com>
2017-05-15 08:01:46 -0700

    AppComponent testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes(
+          [{path: '', component: HomeComponent}])]
+        )
+      ],
       declarations: [
-        AppComponent
+        AppComponent,
+        NavComponent,
+        HomeComponent,
+        FooterComponent
       ],
     }).compileComponents();
   }));
@@ -17,16 +29,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
 });
